d6e283ddd075216581441fc856a145748e6c15c8
Joaquin <joaco.rodriguezortega@gmail.com>
2026-03-20 17:07:49 -0300

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
@@ -7,15 +6,12 @@ import { About } from './components/about/about';
 import { Services } from './components/services/services';
 import { Portfolio } from './components/portfolio/portfolio';
 import { Footer } from './components/footer/footer';
-import { Blog } from './components/blog/blog';
 import { Contact } from './components/contact/contact';
 import { Statistics } from './components/statistics/statistics';
 import { Work } from './components/work/work';
-import { Clients } from './components/clients/clients';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Hero, About, Services, Portfolio, Footer, Blog, Contact, Statistics, Work, Clients],
+  imports: [Navbar, Hero, About, Services, Portfolio, Footer, Contact, Statistics, Work],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
